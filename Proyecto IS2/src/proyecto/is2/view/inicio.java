99be975e2cafe94b730bd79303e0196023d94bda
alumno <alumno@147.156.80.203>
2020-10-06 13:22:01 +0200

    prueba vista inicio

--- Proyecto IS2/src/proyecto/is2/view/inicio.java
+++ Proyecto IS2/src/proyecto/is2/view/inicio.java
@@ -5,10 +5,38 @@
  */
 package proyecto.is2.view;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  *
  * @author carlosguardiola
  */
-public class inicio {
+public class inicio extends JFrame{
+    private JButton boton1, boton2;
+    private JLabel usu,contraseña ;
+    private JLabel vacio, vacio2;
+ 
+    
+    public inicio(){
+        boton1 = new JButton("Entrar");
+        boton2 = new JButton("Registro");
+        usu = new JLabel("Usario");
+        contraseña = new JLabel("Contraseña");
+        vacio = new JLabel();
+        vacio2 = new JLabel();
+        
+        this.setSize(200,300);
+        
+        this.add(boton1);
+        this.add(boton2);
+        this.add(usu);
+        this.add(contraseña);
+        this.add(vacio);
+        this.add(vacio2);
+    
+    }
+    
     
 }
