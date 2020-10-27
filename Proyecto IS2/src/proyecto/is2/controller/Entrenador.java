3ec2c04a17e36e67ed5eee44afbe052e712416fb
macaro22 <macaro22@alumni.uv.es>
2020-10-27 13:06:30 +0100

    creada VentanaAddEntrenador

---
+++ Proyecto IS2/src/proyecto/is2/controller/Entrenador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.is2.controller;
+import proyecto.is2.model.Club; //
+/**
+ *
+ * @author usuario
+ */
+public class Entrenador {
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    //private String club; 
+    
+    public Entrenador(String nombre, String apellidos, int edad){
+        this.nombre=nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+    
+}
